ad046bdce909f5d42a970c280862b56fe8ab2bd2
Ryan Corces <ryancorces@gmail.com>
2021-03-25 11:15:31 -0700

    enable skipping for existing birdseed files

--- SLURM_Demuxlet_makeBirdseed.R
+++ SLURM_Demuxlet_makeBirdseed.R
@@ -96,84 +96,93 @@ colnames(snps) <- c("chr","pos","A","C","G","T")
 snps$chr <- vcf$chr
 snps$pos <- vcf$pos
 
-for (j in 1:nrow(vcf))
-{
-  if(j %% 10000 == 0)
+#if snp_outfile exists, skip this step
+if(!file.exists(snp_outfile)) {
+  for (j in 1:nrow(vcf))
   {
-    print(j)
-  }
-  alleles <- c(vcf$ref[j],unlist(strsplit(vcf$varString[j],",")))
-  counts <- unlist(strsplit(AD[j],","))
-  for(k in 1:length(alleles))
-  {
-    if (alleles[k] == "A")
+    if(j %% 10000 == 0)
     {
-      snps$A[j] <- counts[k]
-    }else if (alleles[k] == "C") {
-      snps$C[j] <- counts[k]
-    }else if (alleles[k] == "G") {
-      snps$G[j] <- counts[k]
-    }else if (alleles[k] == "T") {
-      snps$T[j] <- counts[k]
+      print(j)
+    }
+    alleles <- c(vcf$ref[j],unlist(strsplit(vcf$varString[j],",")))
+    counts <- unlist(strsplit(AD[j],","))
+    for(k in 1:length(alleles))
+    {
+      if (alleles[k] == "A")
+      {
+        snps$A[j] <- counts[k]
+      }else if (alleles[k] == "C") {
+        snps$C[j] <- counts[k]
+      }else if (alleles[k] == "G") {
+        snps$G[j] <- counts[k]
+      }else if (alleles[k] == "T") {
+        snps$T[j] <- counts[k]
+      }
     }
   }
-}
-rownames(snps) <- paste(snps$chr,snps$pos,sep="-")
+  rownames(snps) <- paste(snps$chr,snps$pos,sep="-")
 
-write.table(snps, snp_outfile,sep="\t", col.names = NA, quote = FALSE)
-
-#truncate snp table based off absolute depth of seq at position
-snpsTruncated <- snps[which(rowSums(data.matrix(snps[,c("A","C","G","T")])) > depthCutoff),]
-
-#make new affy-based matrix with one entry per affx snp probe
-affxSNPs <- data.frame(matrix(NA,nrow = nrow(affx), ncol = 8))
-colnames(affxSNPs) <- c("probeID","chr","pos","allele_A","allele_A_counts","allele_B","allele_B_counts","call")
-affxSNPs$probeID <- affx$Probe_Set_ID
-affxSNPs$chr <- affx$chr
-affxSNPs$pos <- affx$stop
-affxSNPs$allele_A <- affx$Allele_A
-affxSNPs$allele_B <- affx$Allele_B
-affxSNPs$call <- rep("-1",nrow(affxSNPs))
-rownames(affxSNPs) <- rownames(affx)
-
-#for each SNP in snps
-for (y in 1:nrow(snpsTruncated))
-{
-  if(y %% 1000 == 0)
-  {
-    print(y)
+  write.table(snps, snp_outfile,sep="\t", col.names = NA, quote = FALSE)
+  } else {
+    snps <- read.table(file = snp_outfile, header = TRUE, sep = "\t", row.names = 1, stringsAsFactors = FALSE)
   }
-  #retrieve affx index position and check that it is a unique match bewtween ATAC snp and AFFX snp
-  affxIndex <- which(rownames(affx) == rownames(snpsTruncated)[y])
-  try(if(length(affxIndex) != 1) stop(paste("ERROR - Duplicate entries identified in AFFX for snp ",rownames(snpsTruncated)[y],"!",sep="")))
+
+
+if(!file.exists(affx_outfile)) {
+  #truncate snp table based off absolute depth of seq at position
+  snpsTruncated <- snps[which(rowSums(data.matrix(snps[,c("A","C","G","T")])) > depthCutoff),]
   
-  #make call 0=AA, 1=AB, 2=BB, -1=no call
-  #if either A or B counts is zero, then the call is easy
-  #if the difference in counts between A and B is lower than 50% of the depth, then its heterzygous
-  alleleA <- affx$Allele_A[affxIndex]
-  alleleB <- affx$Allele_B[affxIndex]
-  alleleA_counts <- as.numeric(snpsTruncated[y,alleleA])
-  alleleB_counts <- as.numeric(snpsTruncated[y,alleleB])
+  #make new affy-based matrix with one entry per affx snp probe
+  affxSNPs <- data.frame(matrix(NA,nrow = nrow(affx), ncol = 8))
+  colnames(affxSNPs) <- c("probeID","chr","pos","allele_A","allele_A_counts","allele_B","allele_B_counts","call")
+  affxSNPs$probeID <- affx$Probe_Set_ID
+  affxSNPs$chr <- affx$chr
+  affxSNPs$pos <- affx$stop
+  affxSNPs$allele_A <- affx$Allele_A
+  affxSNPs$allele_B <- affx$Allele_B
+  affxSNPs$call <- rep("-1",nrow(affxSNPs))
+  rownames(affxSNPs) <- rownames(affx)
   
-  #make call 0=AA, 1=AB, 2=BB, -1=no call
-  #minimum depth = 6 reads
-  if(alleleA_counts == 0) 
+  #for each SNP in snps
+  for (y in 1:nrow(snpsTruncated))
   {
-    currentCall <- 2 #if A = 0 counts, then it must be BB given minimum depth
-  } else if (alleleB_counts == 0) {
-    currentCall <- 0 #if B = 0 counts, then it must be AA given minimum depth
-  } else if ((abs(alleleA_counts - alleleB_counts)/(alleleA_counts + alleleB_counts)) < absAlleleRatio) {
-    currentCall <- 1 #if the counts for A and B are close to each other, it is heterozygous AB
-  } else if (alleleA_counts > alleleB_counts) {
-    currentCall <- 0 #otherwise, if A is greater than B, then it is AA
-  } else if (alleleA_counts < alleleB_counts) {
-    currentCall <- 2 #otherwise, if B is greater than A, then it is BB
-  }#if no call was made by now, the call remains -1
+    if(y %% 1000 == 0)
+    {
+      print(y)
+    }
+    #retrieve affx index position and check that it is a unique match bewtween ATAC snp and AFFX snp
+    affxIndex <- which(rownames(affx) == rownames(snpsTruncated)[y])
+    try(if(length(affxIndex) != 1) stop(paste("ERROR - Duplicate entries identified in AFFX for snp ",rownames(snpsTruncated)[y],"!",sep="")))
+    
+    #make call 0=AA, 1=AB, 2=BB, -1=no call
+    #if either A or B counts is zero, then the call is easy
+    #if the difference in counts between A and B is lower than 50% of the depth, then its heterzygous
+    alleleA <- affx$Allele_A[affxIndex]
+    alleleB <- affx$Allele_B[affxIndex]
+    alleleA_counts <- as.numeric(snpsTruncated[y,alleleA])
+    alleleB_counts <- as.numeric(snpsTruncated[y,alleleB])
+    
+    #make call 0=AA, 1=AB, 2=BB, -1=no call
+    #minimum depth = 6 reads
+    if(alleleA_counts == 0) 
+    {
+      currentCall <- 2 #if A = 0 counts, then it must be BB given minimum depth
+    } else if (alleleB_counts == 0) {
+      currentCall <- 0 #if B = 0 counts, then it must be AA given minimum depth
+    } else if ((abs(alleleA_counts - alleleB_counts)/(alleleA_counts + alleleB_counts)) < absAlleleRatio) {
+      currentCall <- 1 #if the counts for A and B are close to each other, it is heterozygous AB
+    } else if (alleleA_counts > alleleB_counts) {
+      currentCall <- 0 #otherwise, if A is greater than B, then it is AA
+    } else if (alleleA_counts < alleleB_counts) {
+      currentCall <- 2 #otherwise, if B is greater than A, then it is BB
+    }#if no call was made by now, the call remains -1
+    
+    affxSNPs$call[affxIndex] <- currentCall
+    affxSNPs$allele_A_counts[affxIndex] <- alleleA_counts
+    affxSNPs$allele_B_counts[affxIndex] <- alleleB_counts
+  }
   
-  affxSNPs$call[affxIndex] <- currentCall
-  affxSNPs$allele_A_counts[affxIndex] <- alleleA_counts
-  affxSNPs$allele_B_counts[affxIndex] <- alleleB_counts
+  write.table(affxSNPs, affx_outfile,sep="\t", col.names = NA, quote = FALSE)
 }
 
-write.table(affxSNPs, affx_outfile,sep="\t", col.names = NA, quote = FALSE)
-print(paste("Start",date(),sep=" - "))
+print(paste("End",date(),sep=" - "))
